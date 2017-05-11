713a82fcc0af5495a70ea8413659d0ed2ce9d265
zdubzdab <zdubzdab@gmail.com>
2017-05-11 20:16:59 +0300

    Create message and show all messages

--- app/controllers/self_destruction_controller.rb
+++ app/controllers/self_destruction_controller.rb
@@ -2,6 +2,20 @@ require './app/controllers/application_controller'
 
 class SelfDestructionController < ApplicationController
   get '/' do
+    @messages = Message.all
     slim :index
+  end
+
+  post '/submit' do
+    @message = Message.new(params[:message])
+    if @message.save
+      redirect '/messages'
+    else
+      "Sorry, there was an error!"
+    end
+  end
+
+  get '/new' do
+    slim :new
   end
 end
