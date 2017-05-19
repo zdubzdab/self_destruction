5f0e2f47b84c85d42878c23ec9d0d87e7301d5b3
zdubzdab <zdubzdab@gmail.com>
2017-05-19 17:01:21 +0300

    Include css file

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -22,6 +22,7 @@ class ApplicationController < Sinatra::Base
 
   # set folder for templates to ../views, but make the path absolute
   set :views, File.expand_path('../../views', __FILE__)
+  set :public_folder, "public"
 
   # don't enable logging when running tests
   configure :production, :development do
